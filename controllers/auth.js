834fe59b1221ecf09764653de5d5e5170a52c9f2
Rahwa Yohannes <rahwahaile578@gmail.com>
2024-04-22 10:17:59 -0400

    setup login controller function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,48 @@
-const Student = require('../model/Student')
-const jwt = require('jsonwebtoken')
-const bcryptjs = require('bcryptjs')
+const Student = require("../model/Student");
+const jwt = require("jsonwebtoken");
+const bcryptjs = require("bcryptjs");
 
-const register = async (req,res) => {
-    try{
-        const {password} = req.body
-        const salt = await bcryptjs.genSalt(10)
-        const hashedPassword = await bcryptjs.hash(password, salt)
-        const student = await Student.create({...req.body, password: hashedPassword})
-        const token= await jwt.sign({name: student.name, id: student._id}, process.env.JWT_SECRET, {expiresIn: '30d'})
-        res.status(201).json({student, token})
+const register = async (req, res) => {
+  try {
+    const { password } = req.body;
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    const student = await Student.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+    const token = await jwt.sign(
+      { name: student.name, id: student._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" }
+    );
+    res.status(201).json({ student, token });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!password && !email) {
     }
-    catch(error){
-        console.log(error)
+    const student = await Student.findOne({ email });
+    let isMatch = false;
+    if (!student) {
+      res.status(404).json({msg: "student doesn't exist"})
+    }
+    console.log(student)
+    isMatch = await bcryptjs.compare(password, student.password);
+    if (!isMatch) {
+      res.status(404).json({ msg: "Incorrect password" });
     }
     
-   
-    // const token = jwt.sign({student.name,student._id}, 'secret', {expiresIn: '30d'})
-}
-const login = (req, res) => {
+    
+    res.status(201).json({ student });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-}
-
-
-module.exports = {register, login}
+module.exports = { register, login };
