a6cb5cfedc74705f2640cb24a9861ba97b468cb7
Rahwa Yohannes <rahwahaile578@gmail.com>
2024-05-14 10:20:04 -0400

    performed crud on learning groups

--- app.js
+++ app.js
@@ -18,9 +18,7 @@ app.use(express.urlencoded({ extended: false })) //This parses the data and add 
 app.use(express.json())
 
 
-app.use('/api/v1/students', courseRouter, bookmarkRouter, cartRouter, authRouter, followRouter, commentRouter)
-
-
+app.use('/api/v1/students', courseRouter, bookmarkRouter, cartRouter, authRouter, followRouter, commentRouter, learningGroupRouter)
 
 const start =  async ()=>{
     await connectDB(process.env.MONGO_URI)
--- controllers/learningGroup.js
+++ controllers/learningGroup.js
@@ -2,17 +2,75 @@ const LearningGroup = require("../model/LearningGroup");
 
 const createLearningGroup = async (req, res) => {
   try {
-    console.log(req.body)
+    console.log(req.body);
     const learningGroup = await LearningGroup.create({
       ...req.body,
       avatar: req.files["avatar"][0].filename,
       coverPhoto: req.files["coverPhoto"][0].filename,
     });
 
-    res.status(201).json({learningGroup})
+    res.status(201).json({ learningGroup });
   } catch (error) {
     console.log(error);
   }
 };
 
-module.exports = createLearningGroup
+const getAllLearningGroups = async (req, res) => {
+  try {
+    const learningGroups = await LearningGroup.find({});
+    res.status(200).json({ learningGroups });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getLearningGroup = async (req, res) => {
+  try{
+    const learningGroupId = req.params.id 
+    const learningGroup = await LearningGroup.findOne({_id: learningGroupId})
+    if(!learningGroup){
+      res.status(404).json({msg: "learning group not found"})
+    }
+    res.status(200).json({learningGroup})
+  }catch(error){
+    console.log(error)
+  }
+}
+
+const updateLearningGroup = async (req, res) => {
+  try {
+    const learningGroupId = req.params.id;
+    const learningGroup = await LearningGroup.findOneAndUpdate(
+      { _id: learningGroupId },
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (!learningGroup) {
+      res.status(404).json({ msg: "no such learning group" });
+    }
+    res.status(201).json({ learningGroup });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteLearningGroup = async (req, res) => {
+  try {
+    const learningGroupId = req.params.id;
+    const learningGroup = await LearningGroup.findOneAndDelete({
+      _id: learningGroupId,
+    });
+    if(!learningGroup){
+      res.status(404).json({msg: 'learning group is not found'})
+    }
+    res.status(200).json({ msg: "learning group deletion is successful" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//recommended learning groups ... take the courses they are interested in and based on the students enrolled there and their learning groups get them something
+//you could take the course titles as search keywords and recommed sth
+//based on user survey input
+
+module.exports = {createLearningGroup, getAllLearningGroups, getLearningGroup, updateLearningGroup, deleteLearningGroup}
