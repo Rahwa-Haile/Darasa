ac1b861bfe6c050f2ce2cfee020ecc4338b04fe2
Rahwa Yohannes <rahwahaile578@gmail.com>
2024-05-17 13:21:41 -0400

    performed crud on users

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {register, login} = require('../controllers/auth')
+const {register, login, getAllUsers, getUser, updateUser, deleteUser} = require('../controllers/auth')
 
 
 router.post('/register', register)
 router.post('/login', login)
+router.get('/', getAllUsers)
+router.get('/:id', getUser)
+router.patch('/:id', updateUser)
+router.delete('/:id', deleteUser)
 
 module.exports = router
