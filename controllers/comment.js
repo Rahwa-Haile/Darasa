9d60440180e8e4eb0917a677c643368599920f47
Rahwa Yohannes <rahwahaile578@gmail.com>
2024-06-07 00:18:26 -0400

    authorized the comment routes

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,19 +1,35 @@
 const Comment = require("../model/comment");
+const Course = require("../model/course");
 
 const createComment = async (req, res) => {
   try {
     const courseId = req.params.courseId;
     const replyTo = req.query.replyTo;
+    const createdBy = req.user.id
     let replies;
-   
+
+    const course = await Course.findById({ _id: courseId });
+    if (!course) {
+      res.status(404).json({ msg: "Course doesn't exist" });
+    }
     if (replyTo) {
-        const referenceComment = await Comment.findOne({_id: replyTo})
-        replies = referenceComment.numberOfReplies
-        await Comment.findOneAndUpdate({_id: replyTo}, {numberOfReplies: replies + 1}, {new: true, runValidators: true})
+      const referenceComment = await Comment.findOne({ _id: replyTo });
+      if(!referenceComment){
+        res.status(404).json({msg: "reference comment doesn't exist"})
+      }
+      replies = referenceComment.numberOfReplies;
+      await Comment.findOneAndUpdate(
+        { _id: replyTo },
+        { numberOfReplies: replies + 1 },
+        { new: true, runValidators: true }
+      );
     }
     const comment = await Comment.create({
-        ...req.body, courseId, replyTo
-    })
+      ...req.body,
+      createdBy,
+      courseId,
+      replyTo,
+    });
 
     res.status(201).json({ comment });
   } catch (error) {
@@ -25,12 +41,16 @@ const getComment = async (req, res) => {
   try {
     const courseId = req.params.courseId;
     const commentId = req.params.id;
+     const course = await Course.findById({ _id: courseId });
+     if (!course) {
+       res.status(404).json({ msg: "Course doesn't exist" });
+     }
     const comment = await Comment.findOne({ _id: commentId, courseId });
-    const replies = await Comment.find({replyTo: commentId})
+    const replies = await Comment.find({ replyTo: commentId });
     if (!comment) {
       res.status(404).json({ msg: "no comment found" });
     }
-    res.status(200).json({ comment , replies });
+    res.status(200).json({ comment, replies });
   } catch (error) {
     console.log(error);
   }
@@ -38,6 +58,10 @@ const getComment = async (req, res) => {
 const getAllComments = async (req, res) => {
   try {
     const courseId = req.params.courseId;
+     const course = await Course.findById({ _id: courseId });
+     if (!course) {
+       res.status(404).json({ msg: "Course doesn't exist" });
+     }
     const comment = await Comment.find({ courseId }).sort(
       "-numberOfReplies numberOfDislikes -numberOfLikes"
     );
@@ -50,8 +74,13 @@ const getAllComments = async (req, res) => {
 };
 const updateComment = async (req, res) => {
   try {
+    console.log(req.user);
     const courseId = req.params.courseId;
     const commentId = req.params.id;
+     const course = await Course.findById({ _id: courseId });
+     if (!course) {
+       res.status(404).json({ msg: "Course doesn't exist" });
+     }
     const comment = await Comment.findOneAndUpdate(
       { courseId, _id: commentId },
       req.body,
@@ -69,10 +98,15 @@ const deleteComment = async (req, res) => {
   try {
     const courseId = req.params.courseId;
     const commentId = req.params.id;
+     const course = await Course.findById({_id: courseId})
+    if(!course){
+      res.status(404).json({msg: "Course doesn't exist"})
+    }
     const comment = await Comment.findOneAndDelete({
       courseId,
       _id: commentId,
     });
+    
 
     res.status(200).json({ msg: "comment deletion is successful" });
   } catch (error) {
--- routes/comment.js
+++ routes/comment.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const {createComment, getComment, getAllComments, deleteComment, updateComment} = require('../controllers/comment')
+const authenticationMiddleware = require('../middlewares/authenticationMiddleware')
 
-router.post('/comment/:courseId', createComment)
-router.get('/comment/:courseId/:id', getComment)
-router.get('/comment/:courseId', getAllComments)
-router.delete('/comment/:courseId/:id', deleteComment)
-router.patch('/comment/:courseId/:id', updateComment)
+router.post('/comment/:courseId', authenticationMiddleware('Student'), createComment)
+router.get('/comment/:courseId/:id', authenticationMiddleware(), getComment)
+router.get('/comment/:courseId', authenticationMiddleware(), getAllComments)
+router.delete('/comment/:courseId/:id', authenticationMiddleware('Student'), deleteComment)
+router.patch('/comment/:courseId/:id', authenticationMiddleware('Student'), updateComment)
 
 
 
